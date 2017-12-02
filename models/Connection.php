715b5ad08636c7e9137a286c77f59a24404eb0da
root <master@ecaptain.com>
2017-12-02 11:07:28 +0000

    In this commit I add the file with the connection to the BD, was make with mysqli but if you prefer you could change to PDF. -RowerPulido

--- models/Connection.php
+++ models/Connection.php
@@ -1,5 +1,17 @@
 <?php
-    function getConnection(){
+    function getConnection() {
+        $config = parse_ini_file('config.ini.php');
         
+        $host = $config['database']['server'];
+        $user = $config['database']['user'];
+        $password = $config['database']['password'];
+        $database = $config['database']['database'];
+
+        $conn = new mysqli($host, $user, $password , $database);
+        if ($conn->connect_errno) {
+            throw new Exception("Fallo al contenctar a MySQL: (" . $conn->connect_errno . ") " . $conn->connect_error, 1);
+        }
+
+        return $conn;
     }
 ?>
