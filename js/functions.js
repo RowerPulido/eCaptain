a0066fd01b3d284285b91ebaf7110aee194147f0
root <master@ecaptain.com>
2017-12-05 19:27:52 -0800

    All the changes before to do in local mode.  Changes to be committed: 	modified:   css/styles.css 	modified:   index.html 	modified:   js/functions.js 	deleted:    models/Connection.php 	deleted:    models/config.ini.php
     Untracked files:
    	api/
    	script_bd/

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,67 @@
+var dataPoints = [];
+var chart;
+
 function init() {
-    console.log('Hello World!')
+    //console.log("Hola Mundo")
+    chart = new CanvasJS.Chart("graph", {
+        theme: "light",
+        title: {
+            text: "Consumo"
+        },
+        axisX : {
+            valueFormatString : "hh:mm TT",
+            labelAngle : -50,
+            crosshair : {
+                enabled: true,
+                color: "blue",
+                labelFontColor: "#F8F8F8"
+            }
+        },
+        axisY : {
+            valueFormatString : "#,##0.00",
+            crosshair : {
+                enabled: true,
+                color: "orange",
+                labelFontColor: "#F8F8F8"
+            }
+        },
+        data: [{
+            type: "line",
+            dataPoints: dataPoints
+        }]
+    });
+    updateData();
+}
+
+// Initial Values
+var xValue = 0;
+var yValue = 10;
+
+function addData(data) {
+    if(data.STATUS == 0 ){
+        if(data.FOOD_STATUS.length > dataPoints.length) {
+            
+            $.each(data.FOOD_STATUS, function(key, value) {
+                // dataPoints.push({x: value[0], y: parseInt(value[1])});
+                dataPoints.push({x : new Date(value.DATETIME), y: value.FOOD_WEIGHT });
+                xValue++;
+                yValue = parseInt(value.FOOD_WEIGHT);
+            });
+        } else {
+            //dataPoints.shift();
+            
+            // dataPoints.push({x: new Date(data.FOOD_STATUS[0].DATETIME), y: parseInt(data.FOOD_STATUS[0].FOOD_WEIGHT)});
+            // xValue++;
+            // yValue = parseInt(data.FOOD_STATUS[0].FOOD_WEIGHT);
+        }
+        
+        chart.render();
+        setTimeout(updateData, 1500);
+    }
+}
+
+function updateData() {
+    console.log('Entra');
+	$.getJSON("http://165.227.76.225/eCaptain/api/getStatusByIsla.php?ID_ISLAND=1", addData);
+    //addData([[0,6],[1,11],[2,8],[3,12],[4,11],[5,14]]);
 }
