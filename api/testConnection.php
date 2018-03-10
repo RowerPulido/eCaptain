5ffd969cd2b33ee2770155cf65dc7f9357e04393
codigashosVictor <victor_1997_sid@hotmail.com>
2018-03-09 19:58:59 -0800

    baaah

--- api/testConnection.php
+++ api/testConnection.php
@@ -1,12 +1,8 @@
-<<<<<<< HEAD
 <?php
     require_once 'models/Connection.php';
 
     var_dump(getConnection());
-=======
-<?php
     require_once 'models/Connection.php';
 
     var_dump(getConnection());
->>>>>>> origin/master
 ?>
